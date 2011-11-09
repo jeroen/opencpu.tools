9829b4eb6b5cc41ed3fc738b57544abb30afe2c7
Jeroen production <jeroenooms@gmail.com>
2011-11-08 22:25:47 -0800

    Ubuntu 11.10

--- R/runcode.R
+++
@@ -1,9 +0,0 @@
-# TODO: Add comment
-# 
-# Author: jeroen
-###############################################################################
-
-
-runcode <- function(text){
-	return(eval(parse(text=text)));
-}
